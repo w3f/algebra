81fa2921eff511d577096eac6c263c4cb6378b8d
Marcin Górny <marcin.gorny.94@protonmail.com>
2021-11-27 23:45:10 +0100

    Build the Fp^m output elements out from uniform_bytes
    Follow the internet draft 1:1, had to fix previous impl

--- ec/src/hashing/field_hashers/mod.rs
+++ ec/src/hashing/field_hashers/mod.rs
@@ -20,20 +20,6 @@ fn get_len_per_elem<F: Field>(security_parameter: usize) -> usize {
     bytes_per_base_field_elem as usize
 }
 
-//@Skalman: not sure why do we need to that for general bytes that only need to be done by the digest.
-fn map_bytes_to_field_elem<F: Field>(bz: &[u8]) -> Option<F> {
-    let d: usize = F::extension_degree() as usize;
-    let len_per_elem = bz.len() / d;
-    let mut base_field_elems = Vec::new();
-    for i in 0..d {
-        let val = F::BasePrimeField::from_be_bytes_mod_order(
-            &bz[i * len_per_elem..(i + 1) * len_per_elem],
-        );
-        base_field_elems.push(val);
-    }
-    F::from_base_prime_field_elems(&base_field_elems)
-}
-
 /// This field hasher constructs a Hash to Field from a variable output hash function.
 /// It handles domains by hashing the input domain into 256 bits, and prefixes
 /// these bits to every message it computes the hash of.
@@ -60,9 +46,7 @@ pub struct DefaultFieldHasher<H: FixedOutput + Digest + Sized + Clone> {
 }
 
 // Implement HashToField from F and a variable output hash
-impl<F: PrimeField, H: FixedOutput + Digest + Sized + Clone> HashToField<F>
-    for DefaultFieldHasher<H>
-{
+impl<F: Field, H: FixedOutput + Digest + Sized + Clone> HashToField<F> for DefaultFieldHasher<H> {
     fn new_hash_to_field(domain: &[u8], count: usize) -> Result<Self, HashToCurveError> {
         // TODO check whether this is the same as
         // hasher.update(self.domain).update(self.domain.len() as u8);
@@ -95,12 +79,11 @@ impl<F: PrimeField, H: FixedOutput + Digest + Sized + Clone> HashToField<F>
 
         let m = F::extension_degree() as usize;
 
-        // There are a total of `count` elements of F_p^m,
-        // each comprising `m` base field elements.
+        // The user imposes a `count` of elements of F_p^m to output per input msg,
+        // each field element comprising `m` BasePrimeField elements.
         let len_in_bytes = self.count * m * self.len_per_base_elem;
 
         // total length of output divided by output size of the hash function
-        // = number of elements to output
         let ell: usize = (len_in_bytes + b_in_bytes - 1) / b_in_bytes;
 
         // Input block size of sha256
@@ -108,8 +91,7 @@ impl<F: PrimeField, H: FixedOutput + Digest + Sized + Clone> HashToField<F>
         // TODO figure this out
         const l_i_b_str: usize = 128;
 
-        let mut uniform_bytes: Vec<Vec<u8>> = Vec::with_capacity(ell);
-        let mut output: Vec<F> = Vec::with_capacity(ell);
+        let mut uniform_bytes: Vec<u8> = Vec::with_capacity(len_in_bytes);
 
         let mut b_0_hasher = self.hasher.clone();
         b_0_hasher.update(&[0; S_IN_BYTES]);
@@ -125,7 +107,7 @@ impl<F: PrimeField, H: FixedOutput + Digest + Sized + Clone> HashToField<F>
         b_1_hasher.update(&self.domain);
         let b_1 = b_1_hasher.finalize().to_vec();
 
-        uniform_bytes.push(b_1.clone());
+        uniform_bytes.extend(b_1.clone());
 
         let mut b_i = b_1.clone();
         for i in 2..=ell {
@@ -138,11 +120,20 @@ impl<F: PrimeField, H: FixedOutput + Digest + Sized + Clone> HashToField<F>
             b_i_hasher.update(&self.domain);
             // redefine latest b_i
             b_i = b_i_hasher.finalize().to_vec();
-            uniform_bytes.push(b_i.clone());
+            uniform_bytes.extend(b_i.clone());
         }
 
-        for bytes in uniform_bytes.iter() {
-            let f = <F as PrimeField>::from_be_bytes_mod_order(bytes);
+        let mut output: Vec<F> = Vec::with_capacity(self.count);
+        for i in 0..self.count {
+            let mut base_prime_field_elems: Vec<F::BasePrimeField> = Vec::new();
+            for j in 0..m {
+                let elm_offset = self.len_per_base_elem * (j + i * m);
+                let val: F::BasePrimeField = F::BasePrimeField::from_be_bytes_mod_order(
+                    &uniform_bytes[elm_offset..elm_offset + self.len_per_base_elem],
+                );
+                base_prime_field_elems.push(val);
+            }
+            let f: F = F::from_base_prime_field_elems(&base_prime_field_elems).unwrap();
             output.push(f);
         }
 

d9d06773bd2748bcc3a85d30112bd9d1808e7ab1
¨Jeff <burdges@gnunet.org>
2023-04-23 04:28:47 +0200

    Remove curve_maps:: everywhere to shrink later diffs

--- test-curves/src/bls12_381/g1.rs
+++ test-curves/src/bls12_381/g1.rs
@@ -1,6 +1,6 @@
 use crate::bls12_381::*;
 use ark_ec::{
-    hashing::curve_maps::wb::{IsogenyMap, WBConfig},
+    hashing::wb::{IsogenyMap, WBConfig},
     models::CurveConfig,
     short_weierstrass::{self, *},
 };
